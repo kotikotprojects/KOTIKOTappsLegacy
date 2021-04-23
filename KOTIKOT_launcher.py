0f4a46bf3757946e4b1fc97583569bdfeb8125da
BarsTiger <kit.barsic@gmail.com>
2021-04-23 14:01:45 +0300

    1

--- KOTIKOT_launcher.py
+++ KOTIKOT_launcher.py
@@ -3,6 +3,7 @@ import urllib.request, os
 launchfolder = os.getcwd()
 offprojects = launchfolder + "\OfficialProjects"
 launcherfiles = launchfolder + "\!LAUNCHERFILES"
+launchergithuburl = "https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/KOTIKOT_launcher.py"
 
 ################### Checking folders #####################
 if not os.path.exists(offprojects):
@@ -11,4 +12,5 @@ if not os.path.exists(offprojects):
 if not os.path.exists(launcherfiles):
     os.mkdir(launcherfiles)
 
-
+################### Updataing launcher #####################
+urllib.request.urlretrieve(launchergithuburl, "KOTIKOT_launcher.py")

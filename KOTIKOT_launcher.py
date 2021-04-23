79a46000e728f52c70ce2efe3e02acc8451b8aec
BarsTiger <kit.barsic@gmail.com>
2021-04-23 13:59:37 +0300

    1

--- KOTIKOT_launcher.py
+++ KOTIKOT_launcher.py
@@ -2,6 +2,13 @@ import urllib.request, os
 
 launchfolder = os.getcwd()
 offprojects = launchfolder + "\OfficialProjects"
+launcherfiles = launchfolder + "\!LAUNCHERFILES"
 
+################### Checking folders #####################
 if not os.path.exists(offprojects):
     os.mkdir(offprojects)
+
+if not os.path.exists(launcherfiles):
+    os.mkdir(launcherfiles)
+
+

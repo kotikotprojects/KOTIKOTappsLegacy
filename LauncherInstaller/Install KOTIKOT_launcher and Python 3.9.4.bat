e5675bee3bcd37539d4cb1246c20590536b41931
BarsTiger <kit.barsic@gmail.com>
2021-05-07 22:20:09 +0300

    1

--- LauncherInstaller/Install KOTIKOT_launcher and Python 3.9.4.bat
+++ LauncherInstaller/Install KOTIKOT_launcher and Python 3.9.4.bat
@@ -1,4 +1,10 @@
 @echo off
+@echo ----- Downloadig Install-KOTIKOT_launcher from my GitHub ------------------------------------
+powershell -command iwr https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/LauncherInstaller/Install-KOTIKOT_launcher.py -OutFile Install-KOTIKOT_launcher.py
+@echo ----- If you have Python, just close this window now ----------------------------------------
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
 @echo ----- Downloadig Python from official site --------------------------------------------------
 @echo ----- python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe ------------------------------------
 powershell -command iwr https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe -OutFile python-3.9.4-amd64.exe
@@ -9,6 +15,5 @@ ping -n 3 localhost > nul
 ping -n 3 localhost > nul
 ping -n 3 localhost > nul
 ping -n 3 localhost > nul
-@echo ----- Running installer and downloading Install-KOTIKOT_launcher.py ... ---------------------
+@echo ----- Running installer... ------------------------------------------------------------------
 @start python-3.9.4-amd64.exe
-powershell -command iwr https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/LauncherInstaller/Install-KOTIKOT_launcher.py -OutFile Install-KOTIKOT_launcher.py

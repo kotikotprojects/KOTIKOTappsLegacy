4ffb4246016c9fcbc26aba755e1853f382487198
BarsTiger <kit.barsic@gmail.com>
2021-06-01 16:46:44 +0300

    1

--- KOTIKOT_launcher.py
+++ KOTIKOT_launcher.py
@@ -1,4 +1,4 @@
-import urllib.request, os, time
+import urllib.request, os, time, sys
 from subprocess import *
 from PyQt5 import *
 
@@ -17,19 +17,26 @@ if not os.path.exists(launcherfiles):
     os.mkdir(launcherfiles)
 
 ################### Self-updataing launcher #####################
-urllib.request.urlretrieve(launchergithuburl, "KOTIKOT_launcher.py")
+# urllib.request.urlretrieve(launchergithuburl, "KOTIKOT_launcher.py")
 urllib.request.urlretrieve(launcherguigithuburl, launcherfiles + "KOTIKOTlauncherMain.py")
 
 ################### Launching GUI #####################
 # Popen('python ' + launcherfiles + "KOTIKOTlauncherMain.py", shell=True)
-from OfficialProjects.LAUNCHERFILES.KOTIKOTlauncherMain import *
+import OfficialProjects.LAUNCHERFILES.KOTIKOTlauncherMain as kkui
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    KOTIKOTlauncher = QtWidgets.QMainWindow()
-    ui = Ui_KOTIKOTlauncher()
-    ui.setupUi(KOTIKOTlauncher)
-    KOTIKOTlauncher.show()
-    sys.exit(app.exec_())
+app = kkui.QtWidgets.QApplication(sys.argv)
+KOTIKOTlauncher = kkui.QtWidgets.QMainWindow()
+ui = kkui.Ui_KOTIKOTlauncher()
+ui.setupUi(KOTIKOTlauncher)
+KOTIKOTlauncher.show()
 
+################### Launching programs (func) #####################
+def launchAutoShipper():
+    print("meow")
+
+################### Checking buttons #####################
+ui.pushButton_1.clicked.connect(launchAutoShipper)
+
+
+################### Exiting #####################
+sys.exit(app.exec_())

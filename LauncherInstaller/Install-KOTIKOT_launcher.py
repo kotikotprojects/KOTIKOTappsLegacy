48c90534b7e5ad16ed2e9722416433da454524a9
BarsTiger <kit.barsic@gmail.com>
2021-06-01 12:18:36 +0300

    1

--- LauncherInstaller/Install-KOTIKOT_launcher.py
+++ LauncherInstaller/Install-KOTIKOT_launcher.py
@@ -1,10 +1,10 @@
 import urllib.request, os
 
 launchfolder = os.getcwd()
-kkapps = launchfolder + "\KOTIKOT apps"
+kkapps = launchfolder + "/KOTIKOT apps"
 
 if not os.path.exists(kkapps):
     os.mkdir(kkapps)
 
 launcherurl = 'https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/KOTIKOT_launcher.py'
-urllib.request.urlretrieve(launcherurl, kkapps + "\KOTIKOT_launcher.py")
+urllib.request.urlretrieve(launcherurl, kkapps + "/KOTIKOT_launcher.py")

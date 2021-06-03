d31626c8117756a352e42136a1d80a12eabd954a
BarsTiger <kit.barsic@gmail.com>
2021-06-03 18:22:56 +0300

    1

--- LauncherInstaller/Install KOTIKOT_launcher and Python 3.9.5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-brew install wget
-brew install python
-wget https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/LauncherInstaller/Install-KOTIKOT_launcher.py
-python3 Install-KOTIKOT_launcher.py

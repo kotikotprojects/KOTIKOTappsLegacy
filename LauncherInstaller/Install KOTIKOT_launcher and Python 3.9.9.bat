4f87fccc577e6d6b7ce71e2ff7e55224aa79caa7
BarsTiger <kit.barsic@gmail.com>
2021-11-19 21:32:27 +0200

    Updated KOTIKOT_launcher + Py Win10.zip to new bat file with 3.9.9

---
+++ LauncherInstaller/Install KOTIKOT_launcher and Python 3.9.9.bat
@@ -0,0 +1,19 @@
+@echo off
+@echo ----- Downloadig Install-KOTIKOT_launcher from my GitHub ------------------------------------
+powershell -command iwr https://raw.githubusercontent.com/BarsTiger/KOTIKOTapps_download_repo/master/LauncherInstaller/Install-KOTIKOT_launcher.py -OutFile Install-KOTIKOT_launcher.py
+@echo ----- If you have Python, just close this window now ----------------------------------------
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+@echo ----- Downloadig Python from official site --------------------------------------------------
+@echo ----- python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe ------------------------------------
+powershell -command iwr https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe -OutFile python.exe
+@echo ----- After installing python, run Install-KOTIKOT_launcher to install KOTIKOT_launcher -----
+@echo off
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+ping -n 3 localhost > nul
+@echo ----- Running installer... ------------------------------------------------------------------
+@start python.exe
